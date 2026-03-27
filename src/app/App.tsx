3a36b715e13af8c1e656ef678367d778bb41a59c
Irina Yarutkina <irinaarutkina606@gmail.com>
2026-03-27 14:38:28 +0300

    new commit

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/widgets/Header";
+import QuestionCard from "@/components/QuestionCard/QuestionCard";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <QuestionCard />
+    </>
+  );
+}
+
+export default App;
