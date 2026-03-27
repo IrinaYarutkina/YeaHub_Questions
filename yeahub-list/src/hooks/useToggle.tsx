9753950e96ac2c2c1ff646ed6a4f0f78606b31f0
Irina Yarutkina <irinaarutkina606@gmail.com>
2026-03-27 14:21:16 +0300

    Revert "first commit"
    This reverts commit 1f5083579ebbbc95cf269837e7ad48947b8297e3.

--- yeahub-list/src/hooks/useToggle.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-function useToggle(initialValue: boolean) {
-  const [isOpen, setIsOpen] = useState(initialValue);
-
-  const toggle = () => setIsOpen((prev) => !prev);
-  return [isOpen, toggle];
-}
-export default useToggle;
